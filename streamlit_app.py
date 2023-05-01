527e06c5be3b27929cbe8754e8f73ca899bf3578
white-shawdow <105564369+white-shawdow@users.noreply.github.com>
2023-05-01 16:40:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,20 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+    fruit_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruit_normalized)
 
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
-
-fruit_normalized = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruit_normalized)
+except URLErro as e:
+    streamlit.error()
+    
+    
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
