a50d5f112810e9931e1310e75c6c63665db512f1
white-shawdow <105564369+white-shawdow@users.noreply.github.com>
2023-05-01 15:10:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,9 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
-fruit_normalized = pandas.json_normalized(fruityvice_response.json())
+
+fruit_normalized = pandas.json_normalize(fruityvice_response.json())
+
 streamlit.header("Fruityvice Fruit Advice!")
+
 streamlit.dataframe(fruit_normalized)
