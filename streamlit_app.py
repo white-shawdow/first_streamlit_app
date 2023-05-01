342e9a56f9a462697a24c7dcb59631043d94bbce
white-shawdow <105564369+white-shawdow@users.noreply.github.com>
2023-05-01 15:14:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,11 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "fruit_choice")
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/"+ {fruit_choice}")
 
 fruit_normalized = pandas.json_normalize(fruityvice_response.json())
 
